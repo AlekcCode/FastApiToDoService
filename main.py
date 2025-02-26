f16f34d5f788d9966eddbac39539dfb44a701cbd
Alekc Lebdedev <lebedew.alexej2020@gmail.com>
2025-02-26 12:33:47 +0300

    create enpoint and saving data in array

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Optional, Annotated
+
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class STaskAdd(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class STask(STaskAdd):
+    id: int
+
+
+tasks = []
+@app.post('/tasks')
+async def add_tasks(
+        task: Annotated[STaskAdd, Depends()],
+):
+    tasks.append(task)
+    return {'status': 'Ok'}
